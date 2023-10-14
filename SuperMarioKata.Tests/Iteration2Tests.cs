369569e37816bacb211ad02567ff21856b379c4c
Jan <janan9980@gmail.com>
2023-10-14 14:49:10 +0200

    Iteration 2 Tests

--- SuperMarioKata.Tests/Iteration2Tests.cs
+++ SuperMarioKata.Tests/Iteration2Tests.cs
@@ -1,10 +1,40 @@
+using FluentAssertions;
+using SuperMarioKata.MarioZustände;
+
 namespace SuperMarioKata.Tests;
 
 public class Iteration2Tests
 {
     [Fact]
-    public void KleinerSuperMario_Sollte_Bei_Pilzfund_SuperMarioMitPilzSein()
+    public void KleinerSuperMario_Sollte_Bei_Pilzfund_SuperMarioMitPilz_Sein()
     {
+        // Arrange
+        var kleinerMario = new KleinerSuperMario();
         
+        // Act
+        var kleinerMarioFindetPilz = kleinerMario.FindetPilz();
+        
+        // Assert
+        kleinerMarioFindetPilz.Should().BeOfType<SuperMarioMitPilz>();
+    }
+
+    [Fact]
+    public void SuperMarioMitPilz_Sollte_Bei_Pilzfund_SuperMarioMitPilz_Bleiben()
+    {
+        var superMarioMitPilz = new SuperMarioMitPilz();
+
+        var superMarioMitPilzfindetPilz = superMarioMitPilz.FindetPilz();
+
+        superMarioMitPilzfindetPilz.Should().BeOfType<SuperMarioMitPilz>();
+    }
+
+    [Fact]
+    public void SuperMarioMitPilz_Sollte_Bei_Treffer_KleinerSuperMario_Sein()
+    {
+        var superMarioMitPilz = new SuperMarioMitPilz();
+
+        var superMarioMitPilzWirdGetroffen = superMarioMitPilz.WirdGetroffen();
+
+        superMarioMitPilzWirdGetroffen.Should().BeOfType<KleinerSuperMario>();
     }
 }
